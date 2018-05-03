94df70092ecacc3b90c3fa1758edbff32a97ad2c
Dan <dk3790@hotmail.com>
2018-05-03 19:59:02 +0000

    updated budget UI

--- Budgety/app.js
+++ Budgety/app.js
@@ -125,7 +125,11 @@ var UIController = (function()
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
     
     // public methods
@@ -200,6 +204,22 @@ var UIController = (function()
             fieldsArr[0].focus();
         },
         
+        displayBudget: function(obj)
+        {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+            
+            if (obj.percentage > 0)
+            {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            }
+            else
+            {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+        },
+        
         getDOMstrings: function()
         {
             return DOMstrings;
@@ -234,7 +254,7 @@ var controller = (function(budgetCtrl, UICtrl)
         var budget = budgetCtrl.getBudget();
         
         // Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
     
     var ctrlAddItem = function()
@@ -265,6 +285,13 @@ var controller = (function(budgetCtrl, UICtrl)
         init: function()
         {
             console.log("app started");
+            UICtrl.displayBudget(
+            {
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: 0
+            });
             setupEventListeners();
         }
     };
