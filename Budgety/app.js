8555e492188bc1313e9c25b054f59016ce2991b8
Dan <dk3790@hotmail.com>
2018-05-04 16:19:08 +0000

    update ui when deleting an item

--- Budgety/app.js
+++ Budgety/app.js
@@ -206,6 +206,13 @@ var UIController = (function()
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
         
+        deleteListItem: function(selectorID)
+        {
+            var el = document.getElementById(selectorID);
+            
+            el.parentNode.removeChild(el);
+        },
+        
         clearFields: function()
         {
             var fields, fieldsArr;
@@ -317,8 +324,10 @@ var controller = (function(budgetCtrl, UICtrl)
             budgetCtrl.deleteItem(type, ID);
             
             // delete item from UI
+            UICtrl.deleteListItem(itemID);
             
             // update UI
+            updateBudget();
         }
     };
     
